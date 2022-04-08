49016ab95b81b84745d3210009bd21f74c725253
lautaro volcan <lautaroferrandi@gmail.com>
2022-04-08 12:58:20 -0300

    fistDraft

--- validation.js
+++ validation.js
@@ -1,36 +1,21 @@
-
 const contactName = document.getElementById("contactName");
 const email = document.getElementById("email");
 const form = document.getElementById("form");
-const joinButton = document.getElementById("joinButton")
+const joinButton = document.getElementById("joinButton");
 
 //HardCoded Authorization
 let input1 = "lautaro";
-let input2 = "lautaroferrandi@gmail.com"
+let input2 = "lautaroferrandi@gmail.com";
 
-const access = () => {
-    location.href = "https://stuart.com/";
-}
-
-form.addEventListener('submit', (e)=>{
-
-e.preventDefault
-})
-
-const redirect = () =>{
-
-    if(contactName.value == input1 && email.value==input2) {      
-        access();
-        console.log("hey shitty")
-
-        }else{
-            alert("fuckoff")
-        }
+function redirect() {
+  if (contactName.value == input1 && email.value == input2) {
     
+    console.log(contactName.value);
+    setTimeout(() => {
+      location.href = "https://stuart.com/";
+    },2000);
+  } else {
+
+    alert("ERROR");
+  }
 }
-
-
-
-
-
-
