e048527f088448d94ad7f1bc279305e3a96e4084
lautaro volcan <lautaroferrandi@gmail.com>
2022-04-09 15:45:40 -0300

    validarionOK

--- validation.js
+++ validation.js
@@ -4,18 +4,17 @@ const form = document.getElementById("form");
 const joinButton = document.getElementById("joinButton");
 
 //HardCoded Authorization
-let input1 = "lautaro";
-let input2 = "lautaroferrandi@gmail.com";
+let input1 = /^[a-zA-Z ]{2,30}$/;
+let input2 = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 function redirect() {
-  if (contactName.value == input1 && email.value == input2) {
-    
-    console.log(contactName.value);
+  if (contactName.value.match(input1) && email.value.match(input2)) {
+ 
     setTimeout(() => {
       location.href = "https://stuart.com/";
     },2000);
+    
   } else {
-
-    alert("Please fill the form to access");
+      
   }
 }
